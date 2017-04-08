8f98932acc4088546115ca350b907f8e6b3bc8bd
Brian K Tran <bkt5031@gmail.com>
2017-04-08 18:44:58 -0400

    init_7_testing
    init_7_testing

---
+++ BrianTranIST420SPALab/RestTest/Crud.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameShopBrian;
+//using NUnit.Framework;
+
+namespace RestTest
+{
+    [TestClass]
+    public class Crud
+    {
+        
+        private RESTService customer;
+        [TestMethod]
+        public void ShouldReadAll()
+        {
+            ist420grp5Entities1 entities = new ist420grp5Entities1();
+                //Arrange
+                customer = new RESTService();
+            //Act
+            customer.ReadCustomerList();
+
+            //Assert
+
+            Equals(customer.ReadCustomerList());
+        
+ }
+    }
+
+     //   [TestMethod]
+     //   public void ShouldAddCourse()
+     //   {
+     //       //var mockSet = new Mock<DbSet<User>>();
+     //       var mockContext = new Mock<string>(custName, custSSN, custPhone, custEmail);
+
+     //       mockContext.Setup(m => m.Users).Returns(mockSet.Object);
+
+     ////       var usrCRUD = new CreateCustomer(string custName, string custSSN, string
+     ////custEmail, string custPhone);
+            
+     //           //var usr = new User();
+     //           //usr.Login = "Login_Name";
+     //           //usr.Email = "loginName@test.com";
+     //           //usr.Password = "***";
+     //           //usr.InvalidLogins = 0;
+     //           //usr.RememberID = 0;
+
+     //           //usrCRUD.AddUser(usr);
+
+     //           tblCustomerBrianTran cust = new tblCustomerBrianTran();
+     //       cust.CustomerName = custName;
+     //       cust.CustomerSSN = custSSN;
+     //       cust.CustomerEmail = custEmail;
+     //       cust.CustomerPhone = custPhone;
+     //       //Call the method to add the object to the table
+     //       entities.tblCustomerBrianTrans.Add(cust);
+
+     //       mockSet.Verify(m => m.Add(It.Is<User>(arg => arg.Login == "Login_Name")));
+     //       mockContext.Verify(m => m.SaveChanges(), Times.Once());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //private IRESTService cust;
+            ////private RESTService custID;
+            //int custID;
+            //tblCustomerBrianTran cust = new tblCustomerBrianTran();
+            //[TestMethod]
+            //public void GetAllCourses_ShouldReturnAllCourses()
+            //{
+
+            //    //Arrange
+            //    string readUrl = "http://localhost:50491/RESTService.svc/Read?";
+            //    //localHost = new RESTService();
+            //    //var getCourses = ReadCustomerList();
+            //    //var entities = new ist420grp5Entities1();
+            //    //var result = entities.ReadCustomerList() as List<>
+            //    //var getCourses = 1;
+            //    //Act
+            //    //cust.ReadCustomer(int custID);
+            //    readUrl.ReadCustomerList();
+            //    //Assert
+            //    Equals
+
+            //            //Equals(cust.ReadCustomer(int custID));
+
+
+
+            //}
+        }
+
---
+++ BrianTranIST420SPALab/RestTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03e829b4-ecd7-4465-8607-37deac3351b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
